d5f558823f9f63a665158dae6d6e0a318e9cc5e9
ijhwangg <80802650+ijhwangg@users.noreply.github.com>
2023-12-15 08:55:58 +0900

    중괄호 초기화

--- FeverCPP/Prac_01-1_Q4.cpp
+++ FeverCPP/Prac_01-1_Q4.cpp
@@ -4,14 +4,14 @@ using namespace std;
 
 int main()
 {
-	int basic = 50;
+	auto basic = int{ 50 };
 
 	while (true)
 	{
 		cout << "판매 금액을 만원 단위로 입력(-1 to end): ";
 		int sel;
 		cin >> sel;
-		int incen = sel * 0.12;
+		auto incen = sel * 0.12;
 
 		if (0 > sel)
 		{
